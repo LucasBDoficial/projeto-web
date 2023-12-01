8a6bb1dab0bc2b492f164ced0c7efd88f78951da
LucasBD <jlucasbucalon@gmail.com>
2023-12-01 18:28:07 -0300

    image file

--- src/site/components/Clientes.jsx
+++ src/site/components/Clientes.jsx
@@ -8,7 +8,7 @@ export default function Testemunho(){
                     <Carousel.Item className='box' >
                         <img
                         className="testemunho-img"
-                        src="../public/image/cliente.jpg"
+                        src="./public/image/cliente.jpg"
                         alt="Third slide"
                         />
                         <i>João Lucas</i>
@@ -20,7 +20,7 @@ export default function Testemunho(){
                     <Carousel.Item className='box' >
                         <img
                         className="testemunho-img"
-                        src="../public/image/cliente.jpg"
+                        src="./public/image/cliente.jpg"
                         alt="Third slide"
                         />
                         <i>Pedro Henrique</i>
@@ -32,7 +32,7 @@ export default function Testemunho(){
                     <Carousel.Item className='box' >
                         <img
                         className="testemunho-img"
-                        src="../public/image/cliente.jpg"
+                        src="./public/image/cliente.jpg"
                         alt="Third slide"
                         />
                         <i>Fabio Rodrigo</i>
