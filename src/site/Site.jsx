6603020aee16ecc7bad9aae27fdeac5a81c2436b
LucasBD <jlucasbucalon@gmail.com>
2023-12-01 18:09:46 -0300

    vercel problem 01

--- src/site/Site.jsx
+++ src/site/Site.jsx
@@ -1,7 +1,7 @@
 import Banner from "./components/Banner";
 import Menu from "./components/Menu";
 import Features from "./components/Features";
-import Testemunho from "./components/testemunho";
+import Testemunho from "./components/Clientes";
 import Planos from "./components/Planos";
 
 export default function Site() {
