8c8f2c7151b14b7c09608fcddd10fa7761a04ddb
LucasBD <jlucasbucalon@gmail.com>
2023-12-01 22:53:52 -0300

    error pt br

--- src/site/components/Banner.jsx
+++ src/site/components/Banner.jsx
@@ -5,7 +5,7 @@ export default function Banner(){
             <div className="container">
                 <div className="row">
                     <div className="col-lg-6 banner-text">
-                        <h1>Uma plataforma de CRM simples de congigurar e fácil de usar.</h1>
+                        <h1>Uma plataforma de CRM simples de configurar e fácil de usar.</h1>
                         <br />
                 
                         <button type="button" className="btn btn-dark btn-lg btn-banner" >Criar uma conta</button>
