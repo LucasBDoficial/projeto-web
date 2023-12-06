9660198bff5c4770a98f3d4c0518c7dd276261d5
LucasBD <jlucasbucalon@gmail.com>
2023-12-06 09:53:49 -0300

    node 18

--- src/router/ErrorPage.jsx
+++ src/router/ErrorPage.jsx
@@ -1,9 +1,11 @@
 export default function ErrorPage() {
   return (
     <center>
-      <h1>Erro 404!</h1>
       <br />
-      <h2>Página não encontrada</h2>
+      <br />
+      <h1>Erro 404</h1>
+      <br />
+      <h2>Página não encontrada !</h2>
     </center>
   )
 }
