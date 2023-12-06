ca21df4bd5baed2e1a59374145b62efe3f50ff0a
LucasBD <jlucasbucalon@gmail.com>
2023-12-06 20:20:20 -0300

    firebase

---
+++ src/app/config/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDBqCxrt-PcDbFo5iDIicT7iaAAXnDaTFA',
+  authDomain: 'projeto-web-5d62a.firebaseapp.com',
+  projectId: 'projeto-web-5d62a',
+  storageBucket: 'projeto-web-5d62a.appspot.com',
+  messagingSenderId: '1085863111213',
+  appId: '1:1085863111213:web:ef835a83b119a95a6b26a4',
+}
+
+const app = initializeApp(firebaseConfig)
+export const auth = getAuth(app)
--- src/app/login/Login.jsx
+++ src/app/login/Login.jsx
@@ -1,10 +1,41 @@
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+
+import { useState } from 'react'
+import { signInWithEmailAndPassword } from 'firebase/auth'
+import { auth } from './../config/firebase'
 
 import './Login.css'
 
 export default function Login() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const setUser = useNavigate()
+
+  const handleLogin = () => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user
+
+        console.log(user)
+        setUser('/app/home')
+      })
+      .catch((error) => {
+        const errorMessage = error.message
+
+        console.log(errorMessage)
+      })
+  }
+
+  function alterarEmail(event) {
+    setEmail(event.target.value)
+  }
+
+  function alterarPassword(event) {
+    setPassword(event.target.value)
+  }
+
   return (
     <div className="back">
       <div className="container">
@@ -17,6 +48,7 @@ export default function Login() {
 
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Control
+              onChange={alterarEmail}
               className="form-email"
               type="email"
               placeholder="E-mail"
@@ -27,13 +59,19 @@ export default function Login() {
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Control
+              onChange={alterarPassword}
               className="form-pass"
               type="password"
               placeholder="Senha"
             />
           </Form.Group>
 
-          <Button className="form-button" variant="primary" type="submit">
+          <Button
+            onClick={handleLogin}
+            className="form-button"
+            variant="primary"
+            type="button"
+          >
             Entrar
           </Button>
 
